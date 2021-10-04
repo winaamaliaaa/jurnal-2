d07a763d21e6cab0dd31a0f9e928943007599c34
winaamaliaaa <91709580+winaamaliaaa@users.noreply.github.com>
2021-10-04 07:03:39 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+    public class HelloWorld {
+		
+		public static void main(String[] args) {
+			System.out.println("Nama : Wina Amalia");
+			System.out.println("Nim : 6706213153");
+			System.out.println("Kelas : D3RPLA-45-02");
+		}
+	}	
+
+	
